d3b9cbc84ab556baecc62d8ac5622995b25f631c
Anand Tsogtbaatar <atsogt17@MacBook-Pro-2.local>
2019-05-21 02:16:15 -0500

    authentication

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("dotenv").config();
+const session = require("express-session");
+const massive = require("massive");
+const ac = require("../controllers/authController");
+const treasureCtrl = require("../controllers/treasureController");
+const auth = require("./middleware/authMiddleware");
+
+const PORT = 4000;
+
+const { SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("db connected");
+});
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+  })
+);
+
+app.post("/auth/register", ac.register);
+app.post("/auth/register", ac.login);
+app.get("auth/logout", ac.logout);
+
+app.get("/api/treasure/dragon", treasureCtrl.dragonTreasure);
+app.get("/api/treasure/user", auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post("/api/treasure/user", auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get("/api/treasure/all", auth.usersOnly, treasureCtrl.getAllTreasure);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
